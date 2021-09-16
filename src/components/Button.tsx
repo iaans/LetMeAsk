7892d784e3cee1649df3b07d43d7ef205b73e9e1
Ian Augusto Silva <ian.silva@venturus.org.br>
2021-09-16 07:58:50 -0300

    first commit

---
+++ src/components/Button.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+
+export function Button() {
+  // let counter = 0;
+  const [counter, setCounter] = useState(0);
+  function increment() {
+    setCounter(counter + 1);
+  }
+  return <button onClick={increment}>0{counter}</button>;
+}
